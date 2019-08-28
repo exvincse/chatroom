aaeea62ab8d4149eb00099e97e726488e49d136d
yang <exvincse@gmail.com>
2019-08-28 16:44:02 +0800

    456

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -129,6 +129,7 @@ export default {
             //123
             //456
             //789
+            //123
           } else {
             num = Object.keys(name[index].msg).length;
             custom = Object.values(name[index].msg)[num - 1].custom;
